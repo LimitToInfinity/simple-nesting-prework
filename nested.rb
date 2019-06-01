3ab1162802ad19f854508870292c664690ea7a59
Ahmed Gaber <21stlimitless@gmail.com>
2019-06-01 18:17:10 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -131,6 +131,6 @@ def adding_to_dennis
         }
      }
      
-  programmer_hash[:dennis_ritchie][:known_for] = ["Object Orientation", "Assembly"]
+  programmer_hash[:dennis_ritchie][:known_for] = ["Unix", "Assembly"]
   
 end
